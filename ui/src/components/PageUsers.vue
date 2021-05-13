03d351ab7389f409f270bd4f89df44cf86f0f595
Karlos <kajas2015@gmail.com>
2021-05-13 02:10:46 +0200

    ja nevim

--- ui/src/components/PageUsers.vue
+++ ui/src/components/PageUsers.vue
@@ -1,110 +1,132 @@
 <template>
-  <v-container class="px-4">
-    <Title title="Uživatelé" />
-    <v-row>
-      <v-col
-        v-for="user in getAllUsers"
-        :key="user.CustomerID"
-        cols="3"
-      >
-        <v-card @click="redirect(user.CustomerID)">
-          <v-list>
-            <v-list-item>
-              <v-row>
-                <v-col cols="6">
-                  Alias:
-                </v-col>
-                <v-col
-                  cols="6"
-                  class="text-right"
-                >
-                  {{ user.Alias }}
-                </v-col>
-              </v-row>
-            </v-list-item>
-            <v-list-item>
-              <v-row>
-                <v-col cols="6">
-                  Jméno:
-                </v-col>
-                <v-col
-                  cols="6"
-                  class="text-right"
-                >
-                  {{ user.name }} {{ user.surname }}
-                </v-col>
-              </v-row>
-            </v-list-item>
-            <v-list-item>
-              <v-row>
-                <v-col cols="6">
-                  Gender:
-                </v-col>
-                <v-col
-                  cols="6"
-                  class="text-right"
-                >
-                  {{ user.Gender }}
-                </v-col>
-              </v-row>
-            </v-list-item>
-            <v-list-item>
-              <v-row>
-                <v-col cols="6">
-                  Email:
-                </v-col>
-                <v-col
-                  cols="6"
-                  class="text-right"
-                >
-                  {{ user.email }}
-                </v-col>
-              </v-row>
-            </v-list-item>
-            <v-list-item>
-              <v-row>
-                <v-col cols="6">
-                  Telefon:
-                </v-col>
-                <v-col
-                  cols="6"
-                  class="text-right"
-                >
-                  {{ user.phone }}
-                </v-col>
-              </v-row>
-            </v-list-item>
-            <v-list-item>
-              <v-row>
-                <v-col cols="6">
-                  Registrován od:
-                </v-col>
-                <v-col
-                  cols="6"
-                  class="text-right"
-                >
-                  {{ getRegisterDate(user.Registrationdate) }}
-                </v-col>
-              </v-row>
-            </v-list-item>
-            <v-list-item>
-              <v-row>
-                <v-col cols="6">
-                  Oprávnění:
-                </v-col>
-                <v-col
-                  cols="6"
-                  class="text-right"
-                >
-                  {{ user.Access }}
-                </v-col>
-              </v-row>
-            </v-list-item>
-          </v-list>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-row justify="center">
+    <v-col
+      cols="12"
+      md="9"
+    >
+      <v-container class="px-4">
+        <Title title="Uživatelé" />
+        <v-row justify="center">
+          <v-col cols="3">
+            <v-text-field v-model="search" />
+          </v-col>
+        </v-row>
+        <v-data-iterator
+          :items="getAllUsers"
+          :items-per-page="6"
+          :search="search"
+        >
+          <template v-slot:default="props">
+            <v-row>
+              <v-col
+                v-for="user in props.items"
+                :key="user.CustomerID"
+                cols="12"
+                md="6"
+                lg="4"
+              >
+                <v-card @click="redirect(user.CustomerID)">
+                  <v-list>
+                    <v-list-item>
+                      <v-row>
+                        <v-col cols="6">
+                          Alias:
+                        </v-col>
+                        <v-col
+                          cols="6"
+                          class="text-right"
+                        >
+                          {{ user.Alias }}
+                        </v-col>
+                      </v-row>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-row>
+                        <v-col cols="6">
+                          Jméno:
+                        </v-col>
+                        <v-col
+                          cols="6"
+                          class="text-right"
+                        >
+                          {{ user.name }} {{ user.surname }}
+                        </v-col>
+                      </v-row>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-row>
+                        <v-col cols="6">
+                          Gender:
+                        </v-col>
+                        <v-col
+                          cols="6"
+                          class="text-right"
+                        >
+                          {{ user.Gender }}
+                        </v-col>
+                      </v-row>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-row>
+                        <v-col cols="6">
+                          Email:
+                        </v-col>
+                        <v-col
+                          cols="6"
+                          class="text-right"
+                        >
+                          {{ user.email }}
+                        </v-col>
+                      </v-row>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-row>
+                        <v-col cols="6">
+                          Telefon:
+                        </v-col>
+                        <v-col
+                          cols="6"
+                          class="text-right"
+                        >
+                          {{ user.phone }}
+                        </v-col>
+                      </v-row>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-row>
+                        <v-col cols="6">
+                          Registrován od:
+                        </v-col>
+                        <v-col
+                          cols="6"
+                          class="text-right"
+                        >
+                          {{ getRegisterDate(user.Registrationdate) }}
+                        </v-col>
+                      </v-row>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-row>
+                        <v-col cols="6">
+                          Oprávnění:
+                        </v-col>
+                        <v-col
+                          cols="6"
+                          class="text-right"
+                        >
+                          {{ user.Access }}
+                        </v-col>
+                      </v-row>
+                    </v-list-item>
+                  </v-list>
+                </v-card>
+              </v-col>
+            </v-row>
+          </template>
+        </v-data-iterator>
+      </v-container>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -114,6 +136,11 @@ import Title from './layout/Title.vue'
 export default {
   name: 'PageUsers',
   components: { Title },
+  data() {
+    return {
+      search: '',
+    }
+  },
   computed: {
     ...mapGetters(['getAllUsers']),
   },
--- ui/src/components/rooms/ReservationListing.vue
+++ ui/src/components/rooms/ReservationListing.vue
@@ -6,128 +6,143 @@
     >
       <v-container>
         <Title title="Rezervace" />
-        <v-row>
-          <v-col
-            v-for="reservation in getReservations"
-            :key="reservation.ReservationID"
-            cols="12"
-            md="6"
-            lg="4"
-          >
-            <v-card @click="redirect(reservation)">
-              <v-list>
-                <v-list-item>
-                  <v-row>
-                    <v-col cols="6">
-                      <h5 class="title">
-                        Cena:
-                      </h5>
-                    </v-col>
-                    <v-col
-                      cols="6"
-                      class="text-right"
-                    >
-                      <span class="subtitle-1">{{ reservation.Price }}</span>
-                    </v-col>
-                  </v-row>
-                </v-list-item>
-                <v-list-item>
-                  <v-row>
-                    <v-col cols="6">
-                      <h5 class="title">
-                        Počet hostů:
-                      </h5>
-                    </v-col>
-                    <v-col
-                      cols="6"
-                      class="text-right"
-                    >
-                      <span class="subtitle-1">{{ reservation.Numberofguests }}</span>
-                    </v-col>
-                  </v-row>
-                </v-list-item>
-                <v-list-item>
-                  <v-row>
-                    <v-col cols="6">
-                      <h5 class="title">
-                        Jméno:
-                      </h5>
-                    </v-col>
-                    <v-col
-                      cols="6"
-                      class="text-right"
-                    >
-                      <span class="subtitle-1">{{ reservation.contactName }}
-                        {{ reservation.surname }}</span>
-                    </v-col>
-                  </v-row>
-                </v-list-item>
-                <v-list-item>
-                  <v-row>
-                    <v-col cols="6">
-                      <h5 class="title">
-                        Od:
-                      </h5>
-                    </v-col>
-                    <v-col
-                      cols="6"
-                      class="text-right"
-                    >
-                      <span class="subtitle-1">
-                        {{ csLocalizedDateTime(reservation.Startdate) }}
-                      </span>
-                    </v-col>
-                  </v-row>
-                </v-list-item>
-                <v-list-item>
-                  <v-row>
-                    <v-col cols="6">
-                      <h5 class="title">
-                        Do:
-                      </h5>
-                    </v-col>
-                    <v-col
-                      cols="6"
-                      class="text-right"
-                    >
-                      <span class="subtitle-1">{{ csLocalizedDateTime(reservation.Enddate) }}</span>
-                    </v-col>
-                  </v-row>
-                </v-list-item>
-                <v-list-item>
-                  <v-row>
-                    <v-col cols="6">
-                      <h5 class="title">
-                        Hotel:
-                      </h5>
-                    </v-col>
-                    <v-col
-                      cols="6"
-                      class="text-right"
-                    >
-                      <span class="subtitle-1">{{ reservation.name }}</span>
-                    </v-col>
-                  </v-row>
-                </v-list-item>
-                <v-list-item>
-                  <v-row>
-                    <v-col cols="6">
-                      <h5 class="title">
-                        Status:
-                      </h5>
-                    </v-col>
-                    <v-col
-                      cols="6"
-                      class="text-right font-weight-bold"
-                    >
-                      <span class="subtitle-1">{{ getStatus(reservation.Status) }}</span>
-                    </v-col>
-                  </v-row>
-                </v-list-item>
-              </v-list>
-            </v-card>
+        <v-row justify="center">
+          <v-col cols="3">
+            <v-text-field v-model="search" />
           </v-col>
         </v-row>
+        <v-data-iterator
+          :items="getReservations"
+          :items-per-page="6"
+          :search="search"
+        >
+          <template v-slot:default="props">
+            <v-row>
+              <v-col
+                v-for="reservation in props.items"
+                :key="reservation.ReservationID"
+                cols="12"
+                md="6"
+                lg="4"
+              >
+                <v-card @click="redirect(reservation)">
+                  <v-list>
+                    <v-list-item>
+                      <v-row>
+                        <v-col cols="6">
+                          <h5 class="title">
+                            Cena:
+                          </h5>
+                        </v-col>
+                        <v-col
+                          cols="6"
+                          class="text-right"
+                        >
+                          <span class="subtitle-1">{{ reservation.Price }}</span>
+                        </v-col>
+                      </v-row>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-row>
+                        <v-col cols="6">
+                          <h5 class="title">
+                            Počet hostů:
+                          </h5>
+                        </v-col>
+                        <v-col
+                          cols="6"
+                          class="text-right"
+                        >
+                          <span class="subtitle-1">{{ reservation.Numberofguests }}</span>
+                        </v-col>
+                      </v-row>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-row>
+                        <v-col cols="6">
+                          <h5 class="title">
+                            Jméno:
+                          </h5>
+                        </v-col>
+                        <v-col
+                          cols="6"
+                          class="text-right"
+                        >
+                          <span class="subtitle-1">{{ reservation.contactName }}
+                            {{ reservation.surname }}</span>
+                        </v-col>
+                      </v-row>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-row>
+                        <v-col cols="6">
+                          <h5 class="title">
+                            Od:
+                          </h5>
+                        </v-col>
+                        <v-col
+                          cols="6"
+                          class="text-right"
+                        >
+                          <span class="subtitle-1">
+                            {{ csLocalizedDateTime(reservation.Startdate) }}
+                          </span>
+                        </v-col>
+                      </v-row>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-row>
+                        <v-col cols="6">
+                          <h5 class="title">
+                            Do:
+                          </h5>
+                        </v-col>
+                        <v-col
+                          cols="6"
+                          class="text-right"
+                        >
+                          <span class="subtitle-1">
+                            {{ csLocalizedDateTime(reservation.Enddate) }}
+                          </span>
+                        </v-col>
+                      </v-row>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-row>
+                        <v-col cols="6">
+                          <h5 class="title">
+                            Hotel:
+                          </h5>
+                        </v-col>
+                        <v-col
+                          cols="6"
+                          class="text-right"
+                        >
+                          <span class="subtitle-1">{{ reservation.name }}</span>
+                        </v-col>
+                      </v-row>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-row>
+                        <v-col cols="6">
+                          <h5 class="title">
+                            Status:
+                          </h5>
+                        </v-col>
+                        <v-col
+                          cols="6"
+                          class="text-right font-weight-bold"
+                        >
+                          <span class="subtitle-1">{{ getStatus(reservation.Status) }}</span>
+                        </v-col>
+                      </v-row>
+                    </v-list-item>
+                  </v-list>
+                </v-card>
+              </v-col>
+            </v-row>
+          </template>
+        </v-data-iterator>
       </v-container>
     </v-col>
   </v-row>
@@ -140,6 +155,11 @@ import Title from '../layout/Title.vue';
 export default {
   name: 'ReservationListing',
   components: { Title },
+  data() {
+    return {
+      search: '',
+    }
+  },
   computed: {
     ...mapGetters(['getReservations', 'loggedUser']),
   },
